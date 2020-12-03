602fc0a5836c90229252f77d1bf5bfca64d75fc9
Шома Мансуров <shomaman123@gmail.com>
2020-12-03 23:51:06 +0300

    Добавлен конфиг.файл

--- config.php
+++ config.php
@@ -1,1 +1,7 @@
 <?php
+
+/* Настройка загрузки файлов на сервер */
+CONST ALLOW_FILE_TYPES_FOR_DOWNLOAD =
+['jpg'];
+CONST PATH_TO_DOWNLOAD = 'upload/';
+
--- index.php
+++ index.php
@@ -4,9 +4,23 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-use Core\Router;
-use Core\JWT;
-
-require_once 'vendor/autoload.php';
-
-Router::get('/hello/{id}/house/{id}', "TestController@show");
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Загрузка изображений на сервер</title>
+  </head>
+  <body>
+    <form method="post" enctype="multipart/form-data">
+      <input type="file" name="file">
+      <input type="submit" value="Загрузить файл!">
+    </form>
+    <?php
+    // если была произведена отправка формы
+    if(isset($_FILES['file'])) {
+        var_dump($_FILES);
+    }
+    ?>
+  </body>
+</html>
